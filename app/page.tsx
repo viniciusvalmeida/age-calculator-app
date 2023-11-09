192d1d9e63013591b9896489466b7ab249b42889
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-09 17:23:37 -0300

    feat: form html

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
 export default function Home() {
-	return <h1>home</h1>;
+	return (
+		<main className="bg-neutral-white rounded-lg rounded-br-3xl p-8">
+			<div>
+				<form className="flex">
+					<div className="flex flex-col">
+						<label htmlFor="day">DAY</label>
+						<input
+							type="text"
+							name="day"
+							placeholder="DD"
+							className="appearance-none"
+						/>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="month">MONTH</label>
+						<input
+							type="text"
+							name="month"
+							placeholder="MM"
+							className="appearance-none"
+						/>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="year">YEAR</label>
+						<input
+							type="text"
+							name="year"
+							placeholder="YYYY"
+							className="appearance-none"
+						/>
+					</div>
+				</form>
+			</div>
+		</main>
+	);
 }
