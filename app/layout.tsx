d4de584f2ad82d5c3c5974b2fe4193e4d522753f
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-09 16:25:15 -0300

    feat: change default favicon and set layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={poppins.className}>{children}</body>
+			<body
+				className={`${poppins.className} min-h-screen bg-neutral-off-white flex justify-center items-center`}
+			>
+				{children}
+			</body>
 		</html>
 	);
 }
