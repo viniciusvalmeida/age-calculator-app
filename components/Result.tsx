2807181668c7b7ffc42006f978e2d51fe8eee445
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-16 18:47:10 -0300

    dev: responsive result

--- components/Result.tsx
+++ components/Result.tsx
@@ -8,10 +8,10 @@ type ResultProps = {
 export default function Result({ value, type }: ResultProps) {
 	return (
 		<div className="flex space-x-4">
-			<span className="text-primary-purple text-7xl font-bold italic">
+			<span className="text-primary-purple text-6xl md:text-7xl font-bold italic">
 				{value}
 			</span>
-			<span className="text-neutral-off-black text-7xl font-bold italic">
+			<span className="text-neutral-off-black text-6xl md:text-7xl font-bold italic">
 				{type}
 			</span>
 		</div>
