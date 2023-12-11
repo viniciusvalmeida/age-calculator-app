b63d6caa0dabb7a13292c107d60e3f4801c5df0b
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-12-11 15:32:10 -0300

    dev: testing day more than 31

--- __tests__/BirthForm.test.tsx
+++ __tests__/BirthForm.test.tsx
@@ -4,22 +4,32 @@ import BirthForm from "../components/BirthForm";
 import "@testing-library/jest-dom";
 
 describe("BirthForm", () => {
-	it("should be rendered ", () => {
+	beforeEach(() => {
 		render(<BirthForm />);
+	});
 
+	it("should be rendered ", () => {
 		const btn = screen.getByRole("button");
 
 		expect(btn).toBeInTheDocument();
 	});
 
-	it("should show a error message when the day input is empty", async () => {
-		render(<BirthForm />);
-
+	it("should show an error message when day input is empty on submit", async () => {
 		const spanError = screen.getByText("Must be a valid day");
 		const btn = screen.getByRole("button");
 
 		await userEvent.click(btn);
 		expect(spanError).toHaveTextContent("This field is required");
-		// expect(dayElement).toHaveTextContent("32");
+	});
+
+	it("should show an error message when day input is more than 31", async () => {
+		const dayInput = screen.getByLabelText("DAY");
+		const daySpan = screen.getByText("Must be a valid day");
+
+		await userEvent.type(dayInput, "3");
+
+		// const spanStyle = getComputedStyle(daySpan);
+
+		expect(daySpan).not.toBeVisible();
 	});
 });
